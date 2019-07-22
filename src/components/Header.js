13b32d2612196aa355c3036ddd894571d2fe4b0d
Josh Viktorov <joshviktorov27@gmail.com>
2019-07-21 22:11:30 -0400

    Cleaning up and setup

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+        <div class="header">
+            <h1>Welcome to the movie poster collage maker</h1>
+            <p>Get started by clicking on one of the posters and searching for one of your favorite movies!</p>
+        </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/Settings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Settings extends React.Component {
+  render() {
+    return (
+        <div class="settings">
+          <p>This is the settings window</p>
+        </div>
+    );
+  }
+}
+
+export default Settings;
